5b69b664f3958ec91330fc7c1f5cbc4ad447ac48
lizzykim <kjdayoung@gmail.com>
2021-03-14 17:29:46 +0900

    변경사항 없음

--- app/src/main/java/com/example/stt/SpeechToText.kt
+++ app/src/main/java/com/example/stt/SpeechToText.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-
+//추후에 완성
 class SpeechToText : AppCompatActivity() {
 
     lateinit var mgotoTTS: Button
